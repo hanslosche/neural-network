cc64955d54cfde534ab9df4b178d99c548a1eb14
Hans Losche <hansloesche.art@gmail.com>
2020-11-20 15:40:27 -0500

    initial

--- neural-network-01/neural-network-01.py
+++ neural-network-01/neural-network-01.py
@@ -1,9 +1,8 @@
 inputs = [1, 2, 3, 2.5]
 
 weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26 , -0.5]
+weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
-
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
@@ -20,7 +19,7 @@ outputs = [
         inputs[0] * weights2[0] +
         inputs[1] * weights2[1] +
         inputs[2] * weights2[2] +
-        inputs[3] * weights3[3] + bias2,
+        inputs[3] * weights2[3] + bias2,
 
         # Neuron 3:
         inputs[0] * weights3[0] +
@@ -30,4 +29,4 @@ outputs = [
 
 print(outputs)
 
-# 
+#[4.8, 1.21, 2.385]
