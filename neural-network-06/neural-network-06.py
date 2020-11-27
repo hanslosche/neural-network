adbf3cd74d7560968e1ab09dcd47cc362b2fc39a
Hans Losche <hansloesche.art@gmail.com>
2020-11-27 00:22:17 -0500

    'updated'

--- neural-network-06/neural-network-06.py
+++ neural-network-06/neural-network-06.py
@@ -1,17 +1,103 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import vertical_data
 
-# An example output from the output layer
-# of the neural network
+nnfs.init()
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
-softmax_output = [0.7, 0.1, 0.2]
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-# Ground truth
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                                   keepdims = True))
 
-target_output = [1, 0, 0]
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
-loss = -(math.log(softmax_output[0]) * target_output[0] +
-         math.log(softmax_output[1]) * target_output[1] +
-         math.log(softmax_output[2]) * target_output[2])
+        self.output = probabilities
 
-print(loss)
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+            y_pred_clipped*y_true,
+            axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+X, y = vertical_data(samples=100, classes=3)
+
+#Creare model
+dense1 = Layer_Dense(2, 3) # first dense layer, 2 inputs
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+
+#Create loss function
+loss_function = Loss_CategoricalCrossentropy()
+
+
+# some variables to track the best loss
+
+lowest_loss = 99999999 # some initial value
+best_dense1_weights= dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+
+for iteration in range(1000):
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
+
+    # Perform a forward pass of the training data trhough this layer
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    loss = loss_function.calculate(activation2.output, y)
+
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions == y)
+
+    if loss < lowest_loss:
+        print('New set of weights found, iteration:', iteration,
+              'loss', loss, 'acc:', accuracy)
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
